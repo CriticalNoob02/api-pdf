2d225d49dfcc035bdcfcd0084dd0f363f42bf32d
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-05-09 09:26:09 -0300

    feat: add remove file in Reader

--- Reader.py
+++ Reader.py
@@ -1,8 +1,10 @@
 import PyPDF2
+import os
 
 def transcribe(url):
     pdf = open(url, 'rb')
     pdfleitura = PyPDF2.PdfReader(pdf)
     page = pdfleitura.pages[0]
     text = page.extract_text()
+    os.remove(url)
     return text
