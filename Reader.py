dc1695a2c96a3f8ae55f2e4f02c1db91104564b5
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-05-10 14:10:31 -0300

    feat: add comment in Reader file and create loop in pdfFiles

--- Reader.py
+++ Reader.py
@@ -2,9 +2,20 @@ import PyPDF2
 import os
 
 def transcribe(url):
+    # Abrindo arquivo em formato binário;
     pdf = open(url, 'rb')
-    pdfleitura = PyPDF2.PdfReader(pdf)
-    page = pdfleitura.pages[0]
-    text = page.extract_text()
+
+    # Leitor do PDF;
+    readerPDF = PyPDF2.PdfReader(pdf)
+    text = []
+
+    # Loop pelas páginas do PDF;
+    for i in len(readerPDF.pages):
+        page = readerPDF.pages[i]
+
+        # Extração do texto;
+        text.append(page.extract_text())
+
+    # Limpando arquivos já usados
     os.remove(url)
     return text
